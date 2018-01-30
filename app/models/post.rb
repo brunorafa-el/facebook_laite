49c22ea6a01f52eec6c289986112067183804a47
brunorafa-el <brunorafael6656@gmail.com>
2018-01-30 14:28:01 -0200

    Add validates presence

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   has_attached_file :image, styles: { large: "500x500>", medium: "200x200#", thumb: "50x50#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates :caption, presence: true
 
   belongs_to :user
 end
