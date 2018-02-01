e505ec13c32b966664a894f754b1b40b0b6e11fa
brunorafa-el <brunorafael6656@gmail.com>
2018-02-01 13:34:05 -0200

    Update visualisation
    Change some views to a better visualization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.limit(10).order(created_at: :desc)
   end
 
   # GET /posts/1
